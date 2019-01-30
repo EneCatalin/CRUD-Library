1a4bc9defa1b4946a29e8162f6c98f9935c275eb
EneCatalin <enecatalin9393@gmail.com>
2019-01-30 18:11:47 +0200

    found a way to extract the state of each field. Now how do I tie this up

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,19 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {author_name: ''};
+    this.state = {
+      book_title:'',
+      isbn:'',
+      year_published:'',
+      book_description:'',
+      author_name: ''};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({author_name: event.target.value.toUpperCase()});
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   handleSubmit(event) {
@@ -25,21 +30,23 @@ class App extends Component {
     <form className="col s12" onSubmit={this.handleSubmit}>
       <div className="row">
         <div className="input-field col s6">
-          <input id="book_title" type="text" className="validate"/>
+          <input id="book_title" type="text" value={this.state.book_title} onChange={this.handleChange} name="book_title" className="validate"/>
           <label for="book_title">Book Title</label>
         </div>
         <div className="input-field col s6">
-          <input id="author_name" type="text" value={this.state.author_name} onChange={this.handleChange} className="validate"/>
+          <input id="author_name" type="text" value={this.state.author_name} onChange={this.handleChange} className="validate" name="author_name"/>
           <label for="author_name">Author Name</label>
         </div>
       </div>
       <div className="row">
       <div className="input-field col s6">
-          <input id="isbn" type="text" className="validate"/>
+          <input id="isbn" type="text" value={this.state.isbn} 
+          onChange={this.handleChange} className="validate" name="isbn"/>
           <label for="isbn">ISBN</label>
         </div>
         <dov className ="input-field col s6">
-          <input id="publication_year" type="number" className="validate"/>
+          <input id="publication_year" type="number" value={this.state.year_published} 
+          onChange={this.handleChange} className="validate" name="year_published"/>
           <label for="publication_year">Year Pulbished</label>
         </dov>
       </div>
@@ -47,7 +54,8 @@ class App extends Component {
     <form className="col s12">
       <div className="row">
         <div className="input-field col s12">
-          <textarea id="textarea1" className="materialize-textarea"></textarea>
+          <textarea id="textarea1" value={this.state.book_description}
+          onChange={this.handleChange} className="materialize-textarea" name="book_description"></textarea>
           <label for="textarea1">Book Description</label>
         </div>
       </div>
